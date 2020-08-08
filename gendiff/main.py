b68db092097aec19eea81cf0f13d0e0538fe3b3a
doe_deer <gregorylos97@gmail.com>
2020-08-08 18:53:05 +0300

    feat: Add format option to main file

--- gendiff/main.py
+++ gendiff/main.py
@@ -12,10 +12,25 @@ def main():
     parser = argparse.ArgumentParser(
         description='Generate diff between two files in specified format',
     )
-    parser.add_argument('first_file', type=str, help='Source file')
-    parser.add_argument('second_file', type=str, help='Changed file')
+    parser.add_argument('first_file', type=str, help='source file')
+    parser.add_argument('second_file', type=str, help='changed file')
+    parser.add_argument(
+        '-f',
+        '--format',
+        type=str,
+        action='store',
+        choices=['json', 'plain'],
+        default='plain',
+        metavar='FORMAT',
+        help='set format of output. Available choices: plain, json',
+    )
 
     args = parser.parse_args()
+    print('First file: {0}\nSecond file: {1}\nOutput format: {2}'.format(
+        args.first_file,
+        args.second_file,
+        args.format,
+    ))
 
 
 if __name__ == '__main__':
