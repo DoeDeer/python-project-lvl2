ac742c5ec09cc765ca2fb458e90fe53ef01bed1f
doe_deer <gregorylos97@gmail.com>
2020-08-08 20:26:33 +0300

    feat: Add changes format function and realise core func

--- gendiff/gendiff/core.py
+++ gendiff/gendiff/core.py
@@ -3,6 +3,9 @@
 
 """Module with main entities."""
 
+from gendiff.gendiff.files import read_file
+from gendiff.gendiff.out_format import prepare_to_show, changes_to_string
+
 
 def gendiff(ff_path: str, sf_path: str, form: str = 'json') -> str:
     """Generate diff between two files in specified format.
@@ -16,4 +19,8 @@ def gendiff(ff_path: str, sf_path: str, form: str = 'json') -> str:
         str: string with diffs, format as specified.
 
     """
-    pass
+    source = read_file(ff_path)
+    changed = read_file(sf_path)
+
+    changes = prepare_to_show(source, changed)
+    return changes_to_string(changes, form=form)
---
+++ gendiff/gendiff/out_format.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+
+"""Module format files diff."""
+
+import json
+from typing import Optional
+
+from gendiff.gendiff import diff
+
+
+def prepare_to_show(source: dict, changed: dict) -> dict:
+    """Compare to dict and show diffs.
+
+    Args:
+        source (dict): original dict.
+        changed (dict): updated dict.
+
+
+    Returns:
+        dict: dict with union of source and changed keys, but with state prefix.
+
+    """
+    diffs = diff.diff(source, changed)
+    to_format_dict = {}
+
+    for key, state in diffs.items():
+        if state in {diff.ADDED, diff.CHANGED}:
+            to_format_dict['+ {0}'.format(key)] = changed[key]
+
+        if state in {diff.REMOVED, diff.CHANGED}:
+            to_format_dict['- {0}'.format(key)] = source[key]
+
+        if state is diff.UNCHANGED:
+            to_format_dict[key] = source[key]
+
+    return to_format_dict
+
+
+def changes_to_string(changes: dict, form: str = json) -> Optional[str]:
+    """Return pretty-formed string of given object."""
+    if form == json:
+        return json.dumps(changes, indent=2, sort_keys=True)
