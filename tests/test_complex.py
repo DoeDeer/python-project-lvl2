4d40f08feb83c63d8b72fe44a6cd1f94f8818a90
doe_deer <gregorylos97@gmail.com>
2020-08-09 13:56:20 +0300

    teat: Add complex plain test

--- tests/test_complex.py
+++ tests/test_complex.py
@@ -20,6 +20,16 @@ def after_json_complex():
 
 
 @pytest.fixture
+def before_json_complex_plain():
+    return 'tests/fixtures/before_complex_plain.json'
+
+
+@pytest.fixture
+def after_json_complex_plain():
+    return 'tests/fixtures/after_complex_plain.json'
+
+
+@pytest.fixture
 def before_yaml_complex():
     return 'tests/fixtures/before_complex.yml'
 
@@ -47,6 +57,19 @@ def complex_res():
 }"""
 
 
+@pytest.fixture
+def complex_res_plain():
+    return """Property 'common.setting2' was removed
+Property 'common.setting6' was removed
+Property 'common.setting4' was added with value: 'blah blah'
+Property 'common.setting5' was added with value: 'complex value'
+Property 'common.site.base' was removed
+Property 'group1.baz' was changed. From 'bas' to 'bars'
+Property 'group2' was removed
+Property 'group3' was added with value: 'complex value'
+"""
+
+
 def test_complex_json(
     before_json_complex: str,
     after_json_complex: str,
@@ -63,3 +86,16 @@ def test_complex_yaml(
 ):
     """Test complex json files comparing."""
     assert gendiff(before_yaml_complex, after_yaml_complex) == complex_res
+
+
+def test_complex_json_plain(
+    before_json_complex_plain: str,
+    after_json_complex_plain: str,
+    complex_res_plain: str,
+):
+    """Test complex json files comparing."""
+    assert gendiff(
+        before_json_complex_plain,
+        after_json_complex_plain,
+        form='plain',
+    ) == complex_res_plain
